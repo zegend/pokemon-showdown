17dd36198fe1769ba8dc882f0f427d54bce65f11
zegend <zegendoflelda@gmail.com>
2023-07-03 17:11:59 +0100

    add standard natdex to ruleset

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -8,7 +8,7 @@ export const Formats: FormatList = [
 			`&bullet; <a href="https://docs.google.com/spreadsheets/d/1i8GAUogh9yp4bgD0WMeNsuXRkmLk4m7cfgDsE6ba8UY/edit?usp=sharing" target="_blank">OKL Season 5 Sheet</a>`,
 		],
 		mod: 'okl5',
-		ruleset: ['OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod'],
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod'],
 		banlist: [
 			'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'King\'s Rock',
 			'Quick Claw', 'Razor Fang', 'Assist', 'Baton Pass', 'Last Respects', 'Shed Tail',
